31c626db1c73ae6ea037be58d86ac140a03cb8f6
johns224 <johns224@gmail.com>
2017-06-29 11:33:14 -0500

    reformatting

--- src/main/java/org/rossjohnson/homeservices/DoorEvent.java
+++ src/main/java/org/rossjohnson/homeservices/DoorEvent.java
@@ -15,15 +15,16 @@ public class DoorEvent {
 
     @Id
     @GeneratedValue
-	private Long id;
-	private String doorName;
-    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd hh:mm:ss a z", timezone="CDT")
-	private Date eventDate;
-	private Type type;
-	private String homeName;
+    private Long id;
+    private String doorName;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd hh:mm:ss a z", timezone = "CDT")
+    private Date eventDate;
+    private Type type;
+    private String homeName;
 
     // JPA-only
-    DoorEvent() {}
+    DoorEvent() {
+    }
 
     // for Examples only
     public DoorEvent(String homeName, String doorName, Type type) {
@@ -32,58 +33,58 @@ public class DoorEvent {
         this.type = type;
     }
 
-	public DoorEvent(String homeName, String doorName, Type type, Date eventDate) {
+    public DoorEvent(String homeName, String doorName, Type type, Date eventDate) {
         this.doorName = doorName;
         this.homeName = homeName;
         this.type = type;
         this.eventDate = eventDate;
     }
 
-	public DoorEvent(String homeName, String doorName, Date eventDate, Type type) {
-		this.doorName = doorName;
-		this.eventDate = eventDate;
-		this.type = type;
-		this.homeName = homeName;
-	}
+    public DoorEvent(String homeName, String doorName, Date eventDate, Type type) {
+        this.doorName = doorName;
+        this.eventDate = eventDate;
+        this.type = type;
+        this.homeName = homeName;
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public String getDoorName() {
-		return doorName;
-	}
+    public String getDoorName() {
+        return doorName;
+    }
 
-	public void setDoorName(String doorName) {
-		this.doorName = doorName;
-	}
+    public void setDoorName(String doorName) {
+        this.doorName = doorName;
+    }
 
-	public Date getEventDate() {
-		return eventDate;
-	}
+    public Date getEventDate() {
+        return eventDate;
+    }
 
-	public void setEventDate(Date eventDate) {
-		this.eventDate = eventDate;
-	}
+    public void setEventDate(Date eventDate) {
+        this.eventDate = eventDate;
+    }
 
-	public Type getType() {
-		return type;
-	}
+    public Type getType() {
+        return type;
+    }
 
-	public void setType(Type type) {
-		this.type = type;
-	}
+    public void setType(Type type) {
+        this.type = type;
+    }
 
-	public String getHomeName() {
-		return homeName;
-	}
+    public String getHomeName() {
+        return homeName;
+    }
 
-	public void setHomeName(String homeName) {
-		this.homeName = homeName;
-	}
+    public void setHomeName(String homeName) {
+        this.homeName = homeName;
+    }
 
-	public enum Type {
-		opened, closed;
+    public enum Type {
+        opened, closed;
 
-	}
+    }
 }
